1ff80f94c608fdbaa930774b01200cf64335f67b
Benson Diep <bdiep45@gmail.com>
2023-10-04 11:44:56 -0700

    Implemented all functions

--- MCVotingService.java
+++ MCVotingService.java
@@ -1,16 +1,51 @@
 import java.util.Map;
+import java.util.Arrays;
 import java.util.HashMap;
 public class MCVotingService implements VotingService{
 	Question query;
 	Map<String, String> answers;
+	String invalid = "Your answer contains letters that are not \"A, B, C, or D\"";
+	String[] validInputs = {"A", "B", "C", "D", "a", "b", "c", "d"};
+
+	/*
+	 * Creates a VotingService object with a stored question
+	 */
+	public MCVotingService(Question query){
+		this.query = query;
+	}
+
+	/*
+	 * Displays the question and prompts for answers
+	 */
+	@Override
+	public void showQuestion(){
+		query.display();
+	}
 
 	/*
 	 * Iterates through Map answers and counts the instances of A, B, C, D
 	 */
 	@Override
 	public void showTotal() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'showTotal'");
+		int As = 0;
+		int Bs = 0;
+		int Cs = 0;
+		int Ds = 0;
+		for(String s : answers.values()){
+			if(s.contains("A")  || s.contains("a")){
+				As++;
+			}
+			if(s.contains("B")  || s.contains("b")){
+				Bs++;
+			}
+			if(s.contains("C")  || s.contains("c")){
+				Cs++;
+			}
+			if(s.contains("D")  || s.contains("d")){
+				Ds++;
+			}
+		}
+		System.out.println("A: " + As + "\nB: " + Bs + "\nC: " + Cs + "\nD: " + Ds);
 	}
 
 	/*
@@ -19,8 +54,16 @@ public class MCVotingService implements VotingService{
 	 */
 	@Override
 	public boolean verifyAnswer(Student bozo) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'verifyAnswer'");
+		if(!Arrays.stream(validInputs).anyMatch(bozo.getAttempt()::contains)){
+			System.out.println(invalid);
+			return false;
+		}
+		if(answers.containsKey(bozo.getID())){
+			answers.replace(bozo.getID(), bozo.getAttempt());
+		}else{
+			answers.put(bozo.getID(), bozo.getAttempt());
+		}
+		return true;
 	}
 	
 }
